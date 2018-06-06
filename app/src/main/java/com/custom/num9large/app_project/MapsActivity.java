24879026bd91871e39c41cf52803a970032cfb15
John Davies <john.davies@my.jcu.edu.au>
2018-06-06 23:27:42 +1000

    Comment to test Android Studio VCS

--- app/src/main/java/com/custom/num9large/app_project/MapsActivity.java
+++ app/src/main/java/com/custom/num9large/app_project/MapsActivity.java
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+// TEST COMMIT COMMENT
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
